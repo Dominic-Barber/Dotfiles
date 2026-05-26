1f03c985e19819e97161e2f3c53dd2fdf8ff34bc
Dominic Barber <barberdj8@gmail.com>
2026-05-25 23:45:46 -0400

    Arch migration

---
+++ .config/fish/config.fish
@@ -0,0 +1,29 @@
+# Remove the default interactive greeting
+set -U fish_greeting ""
+
+# Initialize Starship prompt
+starship init fish | source
+
+# Handy Aliases
+alias ls="ls --color=auto"
+alias ll="ls -la"
+alias grep="grep --color=auto"
+alias l="ls"
+# Web Dev Aliases
+alias dev="npm run dev"
+alias build="npm run build"
+alias lint="npm run lint"
+
+# Git Aliases
+alias gs="git status"
+alias ga="git add ."
+alias gc="git commit -m"
+
+
+function fish_greeting
+    fastfetch
+end
+
+function config
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
